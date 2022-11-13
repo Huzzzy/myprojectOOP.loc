535e287c0c071b08a327c25d754053d50c9f1c5e
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-11-13 12:06:09 +0300

    Изучение инкапсуляции

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <?php
-$dbh = new \PDO('mysql:host=localhost;dbname=my_db;', 'root', '');
-$dbh->exec('SET NAMES UTF8');
-$stm = $dbh->prepare('SELECT * FROM `users`');
-$stm->execute();
-$allUsers = $stm->fetchAll();
-?>
-<table border="1">
-    <tr><td>id</td><td>Имя</td><td>Email</td></tr>
-    <?php foreach ($allUsers as $user): ?>
-        <tr>
-            <td><?= $user['id'] ?></td>
-            <td><?= $user['name'] ?></td>
-            <td><?= $user['email'] ?></td>
-        </tr>
-    <?php endforeach; ?>
-</table>
+class Cat
+{
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function sayHello()
+    {
+        echo 'Привет! Меня зовут ' . $this->name . '.';
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
+
+$cat1 = new Cat('Снежок');
+
+echo $cat1->getName();
